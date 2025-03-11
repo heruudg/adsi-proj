c5b1ada516281a842533768143b0e7007a9264f7
Heru <hprasetyou@gmail.com>
2025-03-12 01:03:30 +0700

    refactor Table component to improve structure and add pagination links

--- resources/js/components/table.tsx
+++ resources/js/components/table.tsx
@@ -1,5 +1,6 @@
 
 import { TableHeader } from '@/types/component';
+import { Button } from '@/components/ui/button';
 import { Link } from '@inertiajs/react'
 
 interface ResourceDef{
@@ -9,37 +10,49 @@ interface ResourceDef{
 
 export function Table({ header = [], data = [], resourceDef = { resource: '', pk: '' } }: { header: TableHeader[]; data:  any; resourceDef: ResourceDef }) {
     return (
-        <table className='min-w-full'>
-            <thead className='border-gray-100 border-y bg-gray-50 dark:border-gray-800 dark:bg-gray-900'>
-                {header.map((col, index) => (
-                    <th className='px-6 py-3 whitespace-nowrap' key={index}>
-                        <div className='flex items-center'>
-                            {col.title}
-                        </div>
-                    </th>
-                ))}
-                <th>
-                    Actions
-                </th>
-            </thead>
-            <tbody>
-                {data.data.map((row: any, index: number) => (
-                    <tr key={index}>
+        <div>
+       
+            <table className='min-w-full'>
+                <thead className='border-gray-100 border-y bg-gray-50 dark:border-gray-800 dark:bg-gray-900'>
+                    <tr>
                         {header.map((col, index) => (
-                            <td className='px-6 py-3 whitespace-nowrap' key={index}>
+                            <th className='px-6 py-3 whitespace-nowrap' key={index}>
                                 <div className='flex items-center'>
-                                    {row[col.column]}
+                                    {col.title}
+                                </div>
+                            </th>
+                        ))}
+                        <th>
+                            Actions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.data.map((row: any, index: number) => (
+                        <tr key={index}>
+                            {header.map((col, index) => (
+                                <td className='px-6 py-3 whitespace-nowrap' key={index}>
+                                    <div className='flex items-center'>
+                                        {row[col.column]}
+                                    </div>
+                                </td>
+                            ))}
+                            <td className='px-6 py-3 whitespace-nowrap'>
+                                <div className='flex items-center'>
+                                    <Link href={`/${resourceDef.resource}/${row[resourceDef.pk]}`} className='mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Show</Link>
                                 </div>
                             </td>
-                        ))}
-                        <td className='px-6 py-3 whitespace-nowrap'>
-                            <div className='flex items-center'>
-                                <Link href={`/${resourceDef.resource}/${row[resourceDef.pk]}`} className='mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Show</Link>
-                            </div>
-                        </td>
-                    </tr>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div className='mx-auto w-fit mt-4'>
+                {data.links.map((row: any, index: number) => (
+                    row.url ? (
+                        <Link className='mx-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-1' href={row.url} key={index}>{row.label}</Link>
+                    ) : ''
                 ))}
-            </tbody>
-        </table>
+            </div>
+        </div>
     )
 }
