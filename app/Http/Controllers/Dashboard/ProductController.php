9d62af772dad0292f82f3c1e5db4ee3d7db6d844
Heru <hprasetyou@gmail.com>
2025-03-12 11:09:08 +0700

    update .env.example APP_NAME format and add table header structure to ProductController

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -8,4 +8,22 @@ use Illuminate\Http\Request;
 class ProductController extends ResourceController
 {
     //
+    protected $tableHeader = [
+        [
+            "title" => "Inventory Location",
+            "column" => "invt_loc_name",
+        ],
+        [
+            "title" => "Product Name",
+            "column" => "product_name",
+        ],
+        [
+            "title" => "Product Quantity",
+            "column" => "product_quantity",
+        ],
+        [
+            "title" => "Product UOM",
+            "column" => "product_uom",
+        ],
+    ];
 }
